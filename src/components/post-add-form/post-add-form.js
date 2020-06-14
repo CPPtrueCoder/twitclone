62ed617e7ab07b9b0aa19ca799035a11b833771c
CPPtrueCoder <andierodin@gmail.com>
2020-06-15 02:03:14 +0300

    Add form controlling for create new posts

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -4,13 +4,31 @@ import "./post-add-form.css"
 export default class PostAddForm extends Component
 {
 	
+	state={
+	text:''
+	};
 	
+	onValueChanged=(e)=>{
+		this.setState({
+			text:e.target.value
+		})
+	};
+	onSubmit=(e)=>{
+		e.preventDefault();
+		this.props.onAdd(this.state.text)
+	};
 	
 	render() {
 		return(
-			<form className="bottom-panel d-flex">
-				<input className="form-control new-post-label" type="text" placeholder="О чём вы думаете сейчас?"/>
-				<button type="submit" className="btn btn-outline-secondary" onClick={()=>onAdd('Hello')}>
+			<form
+				className="bottom-panel d-flex"
+			onSubmit={this.onSubmit}>
+				<input className="form-control new-post-label"
+				       type="text"
+				       placeholder="О чём вы думаете сейчас?"
+				       onChange={this.onValueChanged}
+				/>
+				<button type="submit" className="btn btn-outline-secondary">
 					Добавить пост</button>
 			
 			</form>
